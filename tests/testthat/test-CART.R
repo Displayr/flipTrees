86ef1c503481e1a968613b7d6fe433312c581232
jakehoare <jake_hoare@hotmail.com>
2017-10-30 09:03:05 +1100

    DS-1526 update test for dot in formula

--- tests/testthat/test-CART.R
+++ tests/testthat/test-CART.R
@@ -10,14 +10,17 @@ levels(bank$fOverall) <- c(levels(bank$fOverall), "8")    # add an empty factor 
 
 test_that("saving variables",
     {
-        z <- CART(Overall ~ Fees + Interest + Phone + Branch + Online + ATM, data = bank, subset = bank$ID > 100)
+        z <- CART(Overall ~ Fees + Interest + Phone + Branch + Online + ATM, data = bank,
+                  subset = bank$ID > 100)
         expect_error(predict(z), NA)
         expect_error(flipData::Probabilities(z))
 
-        z <- suppressWarnings(CART(fOverall ~ Fees + Interest + Phone + Branch + Online + ATM, data = bank, subset = bank$ID > 100))
+        z <- suppressWarnings(CART(fOverall ~ Fees + Interest + Phone + Branch + Online + ATM,
+                                   data = bank, subset = bank$ID > 100))
         expect_error(predict(z), NA)
         expect_error(flipData::Probabilities(z), NA)
     })
+
 
 z <- CART(Overall ~ Fees + Interest + Phone + Branch + Online + ATM, data = bank, subset = bank$ID > 100)
 test_that("rpart prediction",
@@ -25,17 +28,17 @@ test_that("rpart prediction",
         expect_equal(unname(predict(z)[1]), 4.258064516129032)
     })
 
-z <- suppressWarnings(CART(fOverall ~ Fees + Interest + Phone + Branch + Online + ATM, data = bank, subset = bank$ID > 100))
+
+z <- suppressWarnings(CART(fOverall ~ Fees + Interest + Phone + Branch + Online + ATM,
+                           data = bank, subset = bank$ID > 100))
 test_that("rpart Probabilities",
     {
         expect_equal(unname(flipData::Probabilities(z)[1, 4]), 0.2444444444444445)
     })
 
-z <- suppressWarnings(CART(fOverall ~ Fees + Interest + Phone + Branch + Online + ATM, data = bank, subset = bank$ID > 100))
-test_that("dot in formula",
-          {
-              expect_error(CART(yesno ~ ., data = spam7), NA)
-          })
+z <- suppressWarnings(CART(fOverall ~ Fees + Interest + Phone + Branch + Online + ATM,
+                           data = bank, subset = bank$ID > 100))
+
 
 # Reading in the libraries so that their outputs do not pollute the test results.
 library(mice)
@@ -45,16 +48,22 @@ test_that("Error if missing data",
 {
     type = "Sankey"
     # Changing data
-    expect_error((CART(yesno ~ crl.tot + dollar + bang + money + n000 + make, data = spam.sample, missing = "Error if missing data")),NA)
+    expect_error((CART(yesno ~ crl.tot + dollar + bang + money + n000 + make,
+                       data = spam.sample, missing = "Error if missing data")),NA)
     colas$Q32[unclass(colas$Q32) == 1] <- NA
-    expect_that((CART(Q32 ~ Q2, data = colas, subset = TRUE,  missing = "Error if missing data")), (throws_error()))
-    expect_that((CART(Overall ~ Fees + Interest + Phone + Branch + Online + ATM, data = bank, subset = TRUE,  weights = NULL, output = type, missing = "Error if missing data")), (throws_error()))
+    expect_that((CART(Q32 ~ Q2, data = colas, subset = TRUE,  missing = "Error if missing data")),
+                (throws_error()))
+    expect_that((CART(Overall ~ Fees + Interest + Phone + Branch + Online + ATM,
+                      data = bank, subset = TRUE,  weights = NULL, output = type, missing = "Error if missing data")), (throws_error()))
     # filter
-    expect_that((CART(Overall ~ Fees + Interest + Phone + Branch + Online + ATM, data = bank, subset = bank$ID > 100,  weights = NULL, output = type, missing = "Error if missing data")), (throws_error()))
+    expect_that((CART(Overall ~ Fees + Interest + Phone + Branch + Online + ATM,
+                      data = bank, subset = bank$ID > 100,  weights = NULL, output = type, missing = "Error if missing data")), (throws_error()))
     # weight
-    expect_that((CART(Overall ~ Fees + Interest + Phone + Branch + Online + ATM, data = bank, subset = TRUE,  weights = bank$ID, output = type, missing = "Error if missing data")), (throws_error()))
+    expect_that((CART(Overall ~ Fees + Interest + Phone + Branch + Online + ATM,
+                      data = bank, subset = TRUE,  weights = bank$ID, output = type, missing = "Error if missing data")), (throws_error()))
     # weight and filter
-    expect_that((CART(Overall ~ Fees + Interest + Phone + Branch + Online + ATM, data = bank, subset = bank$ID > 100,  weights = bank$ID, missing = "Error if missing")), (throws_error()))
+    expect_that((CART(Overall ~ Fees + Interest + Phone + Branch + Online + ATM,
+                      data = bank, subset = bank$ID > 100,  weights = bank$ID, missing = "Error if missing")), (throws_error()))
     # DS-1525, subset creates empty level of outcome
     expect_error(suppressWarnings(CART(Q32 ~ Q2 + Q3, data = colas, subset = colas$Q32 != "Don't know")), NA)
 })
@@ -67,19 +76,30 @@ for (missing in c("Exclude cases with missing data",
         test_that(paste(missing, type),
         {
             imputation <- missing == "Imputation (replace missing values with estimates)"
-            expect_error((suppressWarnings(CART(yesno ~ crl.tot + dollar + bang + money + n000 + make, data = spam.sample, subset = TRUE,  weights = NULL, output = type, missing = missing))), if (imputation) NULL else NA)
+            expect_error((suppressWarnings(CART(yesno ~ crl.tot + dollar + bang + money + n000 + make,
+                                                data = spam.sample, subset = TRUE,  weights = NULL,
+                                                output = type, missing = missing))),
+                         if (imputation) NULL else NA)
             colas$Q32[unclass(colas$Q32) == 1] <- NA
             colas.small <- colas[, colnames(colas) %in% c("Q32", "Q3", "Q2", "Q4_A", "Q4_B", "Q4_C", "Q11", "Q12")]
             colas.small$Q3[1] <- NA
-            expect_error((suppressWarnings(CART(Q32 ~ Q3, data = colas.small, subset = TRUE,  weights = NULL, output = type, missing = missing))), NA)
-            expect_error((suppressWarnings(CART(Overall ~ Fees + Interest + Phone + Branch + Online + ATM, data = bank, subset = TRUE,  weights = NULL, output = type, missing = missing))), NA)
+            expect_error((suppressWarnings(CART(Q32 ~ Q3, data = colas.small, subset = TRUE,
+                                                weights = NULL, output = type, missing = missing))), NA)
+            expect_error((suppressWarnings(CART(Overall ~ Fees + Interest + Phone + Branch + Online + ATM,
+                                                data = bank, subset = TRUE,  weights = NULL, output = type, missing = missing))), NA)
             # filter
-            expect_error((suppressWarnings(CART(Overall ~ Fees + Interest + Phone + Branch + Online + ATM, data = bank, subset = bank$ID > 100,  weights = NULL, output = type, missing = missing))), NA)
+            expect_error((suppressWarnings(CART(Overall ~ Fees + Interest + Phone + Branch + Online + ATM,
+                                                data = bank, subset = bank$ID > 100,  weights = NULL, output = type,
+                                                missing = missing))), NA)
             # weight
-            expect_error((suppressWarnings(CART(Overall ~ Fees + Interest + Phone + Branch + Online + ATM, data = bank, subset = TRUE,  weights = bank$ID, output = type, missing = missing))), NA)
+            expect_error((suppressWarnings(CART(Overall ~ Fees + Interest + Phone + Branch + Online + ATM,
+                                                data = bank, subset = TRUE,  weights = bank$ID, output = type,
+                                                missing = missing))), NA)
             # weight and filter
-            expect_error((suppressWarnings(CART(Overall ~ Fees + Interest + Phone + Branch + Online + ATM, data = bank, subset = bank$ID > 100,  weights = bank$ID, output = type, missing = missing))), NA)
-        })
+            expect_error((suppressWarnings(CART(Overall ~ Fees + Interest + Phone + Branch + Online + ATM,
+                                                data = bank, subset = bank$ID > 100,  weights = bank$ID,
+                                                output = type, missing = missing))), NA)
+})
 
 
 for (pruning in c("None", "Minimum error", "Smallest tree"))
@@ -90,4 +110,11 @@ for (pruning in c("None", "Minimum error", "Smallest tree"))
                                                 subset = bank$ID > 100, weights = bank$ID,
                                                 output = "Sankey", missing = "Exclude cases with missing data",
                                                 prune = pruning, early.stopping = stopping))), NA)
-            })
+})
+
+
+test_that("CART: dot in formula", {
+    cart <- CART(yesno ~ ., data = spam7)
+    cart2 <- CART(yesno ~ crl.tot + dollar + bang + money + n000 + make, data = spam7)
+    expect_equal(cart, cart2)
+})
