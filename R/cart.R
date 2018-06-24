d9b07e49ca5f2c4010f179b8232a317d5e610196
jakehoare <jake_hoare@hotmail.com>
2018-06-25 09:24:47 +1000

    Add Decision Rules to CART output.

--- R/cart.R
+++ R/cart.R
@@ -342,6 +342,7 @@ Probabilities.CART <- function(object)
 #' @importFrom graphics plot
 #' @importFrom rhtmlSankeyTree SankeyTree
 #' @importFrom rpart plotcp
+#' @importFrom rattle asRules
 #' @export
 print.CART <- function(x, ...)
 {
@@ -375,10 +376,10 @@ print.CART <- function(x, ...)
     {
         plotcp(x, col = 4)
     }
-    #else if (x$output == "Decision Rules")
-    #{
-    #    asRules(x)
-    #}
+    else if (x$output == "Decision Rules")
+    {
+        asRules(x)
+    }
     else
         stop(paste("Unhandled output: ", x$output))
 }
